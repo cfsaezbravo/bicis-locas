4222754b8c42deafc9f7bd2ec7edac758663957f
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-15 13:31:51 -0400

    Validation's alerts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-}
+	var name = document.getElementById("name").value;
+	var lastName = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var password = document.getElementById("input-password").value;
+	var twitter = document.getElementById("input-social").value;
+
+	if(name == null || name.length == 0 || /^\s+$/.test(name)){
+		alert("Escribe algo pajarón");
+	}
+	if(lastName == null || lastName.length == 0 || /^\s+$/.test(lastName)) {
+		alert("Escribe algo pajarón");
+	}
+	if(email == null || email.length == 0 || /^\s+$/.test(email)) {
+		alert("Escribe algo pajarón");
+	}
+	if(password == null || password.length == 0 || /^\s+$/.test(password)) {
+		alert("Escribe algo pajarón");
+	}
+	if(twitter == null || twitter.length == 0 || /^\s+$/.test(password)) {
+		alert("Escribe algo pajarón"); 
+	}
+
+
+};
