05f6b483adb7f7bb7c97582745880cd5150cbe9c
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-15 23:11:44 -0400

    Finish

--- js/main.js
+++ js/main.js
@@ -1,25 +1,78 @@
 function validateForm(){
-	var name = document.getElementById("name").value;
-	var lastName = document.getElementById("lastname").value;
-	var email = document.getElementById("input-email").value;
-	var password = document.getElementById("input-password").value;
-	var twitter = document.getElementById("input-social").value;
+	
+	function nombre(){
+		var nombre = document.getElementById("name").value;
 
-	if(name == null || name.length == 0 || /^\s+$/.test(name)){
-		alert("Escribe algo pajarón");
+		if(!(/^[A-Z][a-z]{3,19}$/).test(nombre)){
+			
+		}
+
+		var divBox = document.getElementsByClassName("name-container input-box")[0];
+		var x = document.createElement("span");
+  		var t = document.createTextNode("Ingrese un nombre válido");
+ 		x.appendChild(t);
+ 		divBox.appendChild(x);
+
+		}
+		nombre();
+
+	function apellido(){
+		var apellido = document.getElementById("lastname").value;
+
+		if(!(/^[A-Z][a-z]{3,19}$/).test(apellido)){
+		
+		} 
+
+		var divBox = document.getElementsByClassName("lastname-container input-box")[0];
+		var x = document.createElement("span");
+  		var t = document.createTextNode("Ingrese un apellido válido");
+ 		x.appendChild(t);
+ 		divBox.appendChild(x);
 	}
-	if(lastName == null || lastName.length == 0 || /^\s+$/.test(lastName)) {
-		alert("Escribe algo pajarón");
-	}
-	if(email == null || email.length == 0 || /^\s+$/.test(email)) {
-		alert("Escribe algo pajarón");
-	}
-	if(password == null || password.length == 0 || /^\s+$/.test(password)) {
-		alert("Escribe algo pajarón");
-	}
-	if(twitter == null || twitter.length == 0 || /^\s+$/.test(password)) {
-		alert("Escribe algo pajarón"); 
+	apellido();
+
+	function email(){
+		var email = document.getElementById("input-email").value;
+
+		if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
+	
+		} 
+
+		var cajaDiv = document.getElementsByClassName("email-container input-box")[0];
+		var x = document.createElement("span");
+  		var t = document.createTextNode("Ingrese un correo valido");
+ 		divBox.appendChild(x);
 	}
 
+	function password(){
+		var password = document.getElementById("input-password").value;
 
-};
+		if((password == "password") || (password == 123456) || (password == 098754) || (!(/^(?=.*\d).{6,}$/).test(password)) ){
+		
+		}	
+
+		var divBox = document.getElementsByClassName("form-group input-box")[0];
+		var x = document.createElement("span");
+  		var t = document.createTextNode("Ingrese una clave válida");
+ 		x.appendChild(t);
+ 		divBox.appendChild(x);
+
+	}
+	password();
+
+	function select(){
+
+		var select = document.getElementsByClassName("form-control").selectedIndex;
+
+		if( select == null || select == 0 ) {
+	  	
+		} 
+
+		var cajaDiv = document.getElementsByClassName("form-group input-box")[1];
+		var x = document.createElement("span");
+  	var t = document.createTextNode("Debe seleccionar su bicicleta");
+ 		x.appendChild(t);
+ 		cajaDiv.appendChild(x);
+	}
+	select();
+}
